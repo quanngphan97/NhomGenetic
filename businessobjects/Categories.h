b079ee1c7f30df71abfd53a5ba104838dcb3a3bd
Ly Truong <truonglehaily@gmail.com>
2020-11-20 14:23:37 +0700

    customers-categories-employees

--- businessobjects/Categories.h
+++ businessobjects/Categories.h
@@ -2,15 +2,20 @@
 #define _Categories_H
 
 #include <string>
+#include "../libs/json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 class Categories{
+public:
 	int CategoryID;
 	string CategoryName, Description;
 public:
 	Categories();
 	Categories(int CategoryID, string CategoryName, string Description);
+	string ToString();
+	json ToJson();
 };
 
 #endif
--- businessobjects/Employees.h
+++ businessobjects/Employees.h
@@ -2,15 +2,20 @@
 #define _Employees_H
 
 #include <string>
+#include "../libs/json.hpp"
 
 using namespace std;
+using json = nlohmann::json;
 
 class Employees{
+public:
 	int EmployeeID;
 	string LastName, FirstName, BirthDate, Photo, Notes;
 public:
 	Employees();
 	Employees(int EmployeeID, string LastName, string FirstName, string BirthDate, string Photo, string Notes);
+	string ToString();
+	json ToJson();
 };
 
 #endif
